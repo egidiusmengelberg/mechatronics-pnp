2208260c86fd4c26d2689e25f3e37a68b1f0f81b
Maximus-Prime66 <107791596+Maximus-Prime66@users.noreply.github.com>
2023-12-22 14:25:52 +0100

    Main heeft nu imports

--- vision/main.py
+++ vision/main.py
@@ -1,1 +1,15 @@
-print("Het vision programma")
+"""Python main
+
+alle functies kunnen vanaf hier aangeroepen worden
+
+
+
+"""
+
+import snap7
+from math import sin, cos, tan, pie, sqrt, atan2
+import numpy as np
+import struct
+import cv2
+import TCP_Connectie as TCP 
+import 
